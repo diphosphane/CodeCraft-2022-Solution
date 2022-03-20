43a4f4d2e93aa9efa41e9687ffd6dc8690ec1254
diphosphane <diphosphane@gmail.com>
2022-03-20 14:03:28 +0800

    from server dispatch revision

--- read_data.py
+++ read_data.py
@@ -3,55 +3,44 @@ from subprocess import getoutput
 
 LOCAL = getoutput('uname') == 'Darwin'
 
-def read_demand() -> Tuple[List[str], List[int]]:
+def read_demand():
     fname = 'data/demand.csv'
-    if not LOCAL:
-        fname = '/' + fname
-    with open(fname) as f:
-        data = f.read().splitlines()
-    client_name = data[0].split(',')[1:]
-    client_demand = []
-    time_label = []
+    if not LOCAL: fname = '/' + fname
+    f = open(fname, 'r'); data = f.readlines(); f.close()
+    client_name = data[0].strip().split(',')[1:]
+    client_demand = []; time_label = []
     for each in data[1:]:
-        d = each.split(',')
+        d = each.strip().split(',')
         time_label.append(d[0])
         client_demand.append(list(map(int, d[1:])))
     return time_label, client_name, client_demand
 
-def read_server_bandwidth() -> Tuple[List[str], List[int]]:
+def read_server_bandwidth():
     fname = 'data/site_bandwidth.csv'
-    if not LOCAL:
-        fname = '/' + fname
-    with open(fname) as f:
-        data = f.read().splitlines()
-    server_name = []
-    server_bandwidth = []
+    if not LOCAL: fname = '/' + fname
+    server_name = []; server_bandwidth = []
+    f = open(fname, 'r'); data = f.readlines(); f.close()
     for each in data[1:]:
-        a, b = each.split(',')
-        server_name.append(a)
-        server_bandwidth.append(int(b))
+        sname, bw4server = each.strip().split(',')
+        server_name.append(sname)
+        server_bandwidth.append(int(bw4server))
     return server_name, server_bandwidth
 
-def read_qos() -> Tuple[List[str], List[str], List[List[int]]]:
+def read_qos():
     fname = 'data/qos.csv'
-    if not LOCAL:
-        fname = '/' + fname
-    with open(fname) as f:
-        data = f.read().splitlines()
-    client_name = data[0].split(',')[1:]
-    server_name = []
-    qos_array = []
+    if not LOCAL: fname = '/' + fname
+    f = open(fname, 'r'); data = f.readlines(); f.close()
+    cname = data[0].strip().split(',')[1:]
+    sname = []; qos_array4server = []
     for each in data[1:]:
-        d = each.split(',')
-        server_name.append(d[0])
-        qos_array.append(list(map(int, d[1:])))
-    return client_name, server_name, qos_array
+        qos_line_split = each.strip().split(',')
+        sname.append(qos_line_split[0])
+        qos_array4server.append(list(map(int, qos_line_split[1:])))
+    return cname, sname, qos_array4server
 
-def read_qos_limit() -> int:
+def read_qos_limit():
     fname = 'data/config.ini'
-    if not LOCAL:
-        fname = '/' + fname
-    with open(fname) as f:
-        data = f.read().splitlines()
-    qos_lim = int(data[1].split('=')[1])
+    if not LOCAL: fname = '/' + fname
+    f = open(fname, 'r'); data = f.readlines(); f.close()
+    qos_lim = int(data[1].strip().split('=')[1])
     return qos_lim
